34a238633fe48b03b8f3ab57eb8252a40927d242
ajpicard913 <Contact@saucedesigns.io>
2020-11-15 01:00:09 -0500

    Initial Commit

---
+++ Shared/welcomeHOmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcomeHOmeApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct welcomeHOmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
